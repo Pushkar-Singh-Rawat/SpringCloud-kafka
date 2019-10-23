847449a431c2fee22cbcc3f32963283211c71e53
Pushkar Singh Rawat <pushkars@amdocs.com>
2019-10-23 20:31:35 +0530

    added model class for message

---
+++ src/main/java/springkafka/kafkademo/messagemodel/Message.java
@@ -0,0 +1,16 @@
+package springkafka.kafkademo.messagemodel;
+
+public class Message {
+
+	private String sampleMessage;
+
+	public String getSampleMessage() {
+		return sampleMessage;
+	}
+
+	public void setSampleMessage(String sampleMessage) {
+		this.sampleMessage = sampleMessage;
+	}
+	
+	
+}
